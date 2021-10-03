d12da5c3523a1669c47b695eaadcb957f3f41ea6
lucca <luccagugescu@gmail.com>
2021-10-03 16:09:21 +0200

    add setup env

---
+++ suspro/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse,JsonResponse
+import matplotlib.pyplot as plt
+from pycalphad import Database, binplot
+import pycalphad.variables as v
+import matplotlib.pyplot as plt
+from io import StringIO
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+import os 
+
+def saludo(request):
+    
+    # Compute the phase diagram and plot it on the existing axes using the `plot_kwargs={'ax': axes}` keyword argument
+    return JsonResponse({"f": "f"})
+    #plt.show()
+    
+
+
+def Calculaentalpia(request,temperatura,presion):
+    BASE_DIR = os.getcwd()
+    path1 = BASE_DIR + "/suspro/Fe-C.TDB"
+    # Load database and choose the phases that will be considered
+    db_alzn = Database(path1)
+    my_phases_alzn = ['LIQUID', 'GRAPHITE', 'BCC_A2']
+    # Create a matplotlib Figure object and get the active Axes
+    fig = plt.figure(figsize=(9,6))
+    axes = fig.gca()
+    binplot(db_alzn, ['C', 'Fe', 'VA'] , my_phases_alzn, {v.X('C'):(0,1,0.02), v.T: (300, 10000, 10), v.P:101325, v.N: 1}, plot_kwargs={'ax': axes})
+    response = HttpResponse(content_type = 'image/png')
+    canvas = FigureCanvasAgg(fig)
+    return response
+
+def objetoJ(request):
+    casa='5'
+    return JsonResponse({'casa':casa})
